e3c17e7458ee0be4ffd5b6cb5a81118d73d1b5f3
Trevahr Robertson <Trevahr@gmail.com>
2019-08-08 09:34:28 -0400

    DQ 'Done'

--- src/index.js
+++ src/index.js
@@ -3,4 +3,46 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+
+  fetch('https://randomuser.me/api/')
+  .then(res => res.json())
+  .then( res => {
+      generateUser(res)
+    
+  }
+
+  )
+
+ function generateUser(res){
+    
+    document.querySelector('#email').innerText = res['results'][0].email
+    document.querySelector('#phone').innerText = res['results'][0].phone
+    document.querySelector('#cell').innerText = res['results'][0].cell
+    document.querySelector('#fullname').innerText = `${res['results'][0].name.title}. ${res['results'][0].name.first} ${res['results'][0].name.last}`
+    document.querySelector('#date_of_birth').innerText = res['results'][0].dob.date
+    document.querySelector('#street').innerText = res['results'][0].location.street
+    document.querySelector('#city').innerText = res['results'][0].location.city
+    document.querySelector('#state').innerText = res['results'][0].location.state
+    document.querySelector('#postcode').innerText = res['results'][0].location.postcode
+ }
+
+  button = document.getElementsByClassName('btn btn-primary')
+    button.addEventListener('click', generateUser)
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
